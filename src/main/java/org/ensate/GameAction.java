3d7eb3f42daa19aaff561a62acf952b51859ff76
essebaiyayaa <essebaiyaya@gmail.com>
2025-05-14 21:30:56 +0100

    shooting game

---
+++ src/main/java/org/ensate/GameAction.java
@@ -0,0 +1,21 @@
+package org.ensate;
+import java.io.Serializable;
+
+public class GameAction implements Serializable {
+    public String action;
+    public int x, y;
+    public String actionData;
+
+    public GameAction(String action, int x, int y) {
+        this.action = action;
+        this.x = x;
+        this.y = y;
+    }
+
+    public GameAction(String action, int x, int y, String actionData) {
+        this.action = action;
+        this.x = x;
+        this.y = y;
+        this.actionData = actionData;
+    }
+} 
